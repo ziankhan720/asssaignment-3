5d61503c6104cd22e04e25c250ca6be9bde7ab30
ziankhan720 <Zk167854@gmail.com>
2025-02-27 12:56:55 +0500

    Add files via upload

---
+++ Assignment no 3.py
@@ -0,0 +1,21 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #activity 1:
+>>> n=input("enter a number")
+enter a number20
+>>> if int(n)%2==0:
+...     print("the given number is an even number")
+... 
+...     
+the given number is an even number
+>>> 
+>>> #activity 2:
+>>> n=input("enter a number")
+enter a number36
+>>> if int(n)%2==0:
+...     print("the given number is an even number")
+... else:
+...     print("the given number is an odd number")
+... 
+...     
+the given number is an even number
